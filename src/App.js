ac4ef1cfa25f5069b6abaa9a2912ff82d5817d68
Restyled.io <commits@restyled.io>
2021-06-27 12:28:24 +0000

    Restyled by prettier

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import './App.css';
+import "./App.css";
 
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hemanth Portfolio
-        </p>
+        <p>Hemanth Portfolio</p>
         <a
           className="App-link"
           href="https://reactjs.org"
-  target = "_blank"
-  rel = "noopener noreferrer" > Learn React</a>
-      </header><
-        /div>
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
